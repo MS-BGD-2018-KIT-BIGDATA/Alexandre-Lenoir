eb40210cd6b76d5b92bf2d0072628eb28c6ed02b
Alexandre Lenoir <alexandre.lenoir@telecom-paristech.fr>
2017-10-05 23:24:45 +0200

    ordering contributors by their start count average over repositories

--- Lesson3/exo_dom_lesson3_github_top_contributors.py
+++ Lesson3/exo_dom_lesson3_github_top_contributors.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from secret.secretstore import TOKEN
+import pandas as pd
+from multiprocessing import Pool
+
 
 def getRepositoriesOfUser(user):
     request_link = "https://api.github.com/users/" + user + "/repos"
@@ -31,7 +34,15 @@ def getRepositoriesOfUser(user):
             request_link = content.links['next']['url']
         else:
             request_link = None
-    return repo_count, repo_watchers / repo_count
+    return repo_count, repo_watchers
+
+
+def scrap_github(arguments):
+    rank, contributor = arguments
+    repo_count, repo_watchers = getRepositoriesOfUser(contributor['name'])
+    print("Contributor <{}> ranked {} owns {} repositories with an average star count of {:.2f}.".format(
+        contributor['name'], rank + 1, repo_count, repo_watchers / repo_count if repo_count != 0 else 0))
+    return [contributor['name'], repo_count, repo_watchers]
 
 
 def main():
@@ -40,17 +51,23 @@ def main():
     soup = BeautifulSoup(content.text, 'html.parser')
     table_entries = soup.find_all("tr", limit=257)
     top_contributors = []
-    for i in range(1, 257):
+    for i in range(1, 8):
         link = table_entries[i].find("a")
         url = link.get("href")
         name = link.string
         top_contributors.append({'name': name, 'url': url})
 
-    for rank, contributor in enumerate(top_contributors):
-        repo_count, average_watchers = getRepositoriesOfUser(
-            contributor['name'])
-        print("Contributor <{}> ranked {} owns {} repositories with an average star count of {:.2f}.".format(
-            contributor['name'], rank + 1, repo_count, average_watchers))
+    df = []
+    input_data = [(rank, contributor)
+                  for rank, contributor in enumerate(top_contributors)]
+    for single_data in input_data:
+        df.append(scrap_github(single_data))
+
+    df = pd.DataFrame(data=df, columns=["name", "repo_count", "repo_watchers"])
+    df.to_csv("contributors.csv")
+    df['average_watchers'] = df['repo_watchers'] / df['repo_count']
+    df = df.sort_values(by='average_watchers', ascending=False)
+    print(df.head())
 
 
 if __name__ == '__main__':
